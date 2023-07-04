f1a00477709d14c7f142feb990d2aa149c702b8c
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2023-07-04 15:35:44 -0300

    commit inicial

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Países</h1>
+      <ul>
+        <li>1 - Brasil (BR)</li>
+        <li>1 - Brasil (BR)</li>
+        <li>1 - Brasil (BR)</li>
+        <li>1 - Brasil (BR)</li>
+      </ul>
+    </div>
+  );
+}
