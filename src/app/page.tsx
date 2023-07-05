91ebd5c4ca609a11c1caf8c416daf4f1ec2f1b00
Lucas Pacheco Teixeira <lucas@eliti.com.br>
2023-07-05 07:47:47 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,10 @@ import Form from "./form";
 
 export default function Home() {
   return (
-    <div className="m-4 flex flex-col gap-2">
-      <h1>Países</h1>
+    <div className="p-4 flex flex-col gap-2 container mx-auto">
+      <h1 className="uppercase font-light tracking-[1em] text-center opacity-30">
+        Países
+      </h1>
       <Lista />
       <Form />
     </div>
